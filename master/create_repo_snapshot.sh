8345fc69e478a9785320432d01e127ee5ec6955b
Hans Then <hans.then@gmail.com>
2016-04-22 00:02:18 +0200

    Cleanup and made drbd use latest versions again.

--- master/create_repo_snapshot.sh
+++ master/create_repo_snapshot.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -x
-OSTACKRELEASE="openstack-liberty"
+OSTACKRELEASE="centos-openstack-liberty"
 DATE=$(date +%Y%m%d)
 set +x
-if ! grep -Fq $OSTACKRELEASE /etc/yum.repos.d/rdo-release.repo; then
+if ! grep -Fq $OSTACKRELEASE /etc/yum.repos.d/*.repo; then
     echo "Please check that your rdo-release package matches the $OSTACKRELEASE distribution!"
-    echo "You can find the package in https://repos.fedorapeople.org/repos/openstack/$OSTACKRELEASE/"
     exit 1
 fi
 set -x
@@ -17,8 +16,7 @@ if [ -d /install/post/otherpkgs/centos7/x86_64/ ]; then
     # Remove the symlink
     rm /install/post/otherpkgs
 fi
-reposync -n -r epel -r base -r extras -r updates -r xcat-2-core -r xcat-dep -r $OSTACKRELEASE -p /install/post/$DATE/centos7/x86_64/
-reposync -r elrepo -p /install/post/$DATE/centos7/x86_64/
+reposync -n -r epel -r base -r extras -r updates -r xcat-2-core -r xcat-dep -r elrepo -r $OSTACKRELEASE -p /install/post/$DATE/centos7/x86_64/
 # Sync the repo that is in the $DATE directory, downloading only what is missing (faster)
 ln -sT /install/post/$DATE /install/post/otherpkgs
 createrepo /install/post/otherpkgs/centos7/x86_64/
